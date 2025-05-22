6fa1479814ffcda688875f4e8138d903307bb7a7
Vedant Bhatt <vickyvedant365@gmail.com>
2025-05-22 22:10:38 +0530

    As we know settings.py is global file its will be alwys one in the myproject files , here we resiter the 'posts' django app(django module) by the means of adding it to the installed_apps list.

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts'  # here we register out 'posts' app here in setting.py by adding it to the installed app list
 ]
 
 MIDDLEWARE = [
