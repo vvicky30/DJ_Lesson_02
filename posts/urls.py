9cdba2c6d82e23ea54d055745a26b8c8b934362f
Vedant Bhatt <vickyvedant365@gmail.com>
2025-05-23 22:03:17 +0530

    here in the url file dedicated to 'post' app we created the named-url by adding name parameter while giving path in url_patterns list.

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views   # importing views for giving path to web pages using urlpatterns(routes)
 
 urlpatterns = [
-    path('', views.posts_list ),    # here we added the path/route to posts_list views which is in the views.py(dedicated to posts app only) 
+    path('', views.posts_list, name= "posts" ),    # here we added the path/route to posts_list views which is in the views.py(dedicated to posts app only) 
+                                                  #here we also added name paramter in giving path to views posts_list ,this because we want to make this url as named-url ,here it's named as "posts"
 ]
