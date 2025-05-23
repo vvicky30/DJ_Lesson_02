758f7f6e764ffc7d5e710a70e49601cdf5f64ac7
Vedant Bhatt <vickyvedant365@gmail.com>
2025-05-23 12:03:01 +0530

    here this tme we're creating object/instance of our data model class 'Post' (in interactive shell of django) and with the help of this insatance add title to our firsst and second post and saves it , and retrive it with 'Post.objects.all()' and also made instance function for better retirval (with values, posts content instead of numbers).

--- posts/models.py
+++ posts/models.py
@@ -11,7 +11,8 @@ class Post(models.Model):
     slug = models.SlugField() # this will use to identify each post with user input slug(tag) which will be added at the end of url (this is compulsory)
     date = models.DateTimeField(auto_now_add=True) #auto_now_add=True means it will add system's datetime stamp when user added or saves post at web automatically. 
 
-
+    def __str__(self):  # here we make instance function so during retrival it will show object's value or actual posts instead of number of posts 
+        return self.title 
 
 #once we create our data model we have to apply for migration for our created data model in cmd terminal before we runserver.
 # for that we have to create migration first  ; cmd : Dj_lesson_02\myproject>py manage.py makemigrations ; this will create migration file corresponds to our post data model with tracking id like 001 (this tracking id used to track potential modifications in dats model)
@@ -27,3 +28,40 @@ Running migrations:
 
 # [NOTE]: myproject(upper directory) has its own sets of standard data models for thir template database and meta-data purposes, so for this first we have to apply migration for such standard data models of project and then we can apply it for django app i.e. posts.
           #command to do that (at myproject directory) : Dj_lesson_02\myproject>py manage.py migrate
+
+''' 
+what is ORM : ORM is stands for object relational mapping , as we know our data model which we're building here will saves that categorical data from front-end end-use to the corresponding table with same name as data-model in the db at back-end
+so basically here data model class that we made 'Post' will acts as table name in the db server which stores data.
+with the help of ORM its facilitates the movements of data b/w our data models(which take categorical data from front-end) and table in the db at back-end 
+for ORM we have to make instance of our data model class which's here 'Posts' 
+then we can acess the standard functions(model-reference filds) of class(data model) using this instance/object 
+we can also make the string type return instance function which gives us actual content in the data model class instead of numbers.
+
+here is simple illustration in interactive shell for django project 
+Dj_lesson_02\myproject> py manage.py shell
+
+7 objects imported automatically (use -v 2 for details).
+
+Python 3.12.10 (tags/v3.12.10:0cc8128, Apr  8 2025, 12:21:36) [MSC v.1943 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.   
+(InteractiveConsole)
+>>> from posts.models import Post    # we import our data model class from posts/models.py file  
+>>> p= Post()  # here make instanace/Object data model class Post
+>>> p     # inspect p ;    which is of object type referring to Post data model class
+<Post: Post object (None)>
+>>> p.title = "My first post"  #giving now title to first post by using instance to acess data model class's model reference field 
+>>> p.save()  # saving it 
+>>> Post.objects.all()  # retrivng all the objects for seeing the values of post 
+<QuerySet [<Post: Post object (1)>]>  # here it showing there's only one 
+>>> exit()   
+''' 
+#in interactive shell of django project
+'''
+so after making instance str function in our data model class; we can now see actual posts while retreiving the objects value instead of numbers: 
+>>> from posts.models import Post
+>>> p= Post()
+>>> p.title = "My second post"
+>>> p.save()
+>>> Post.objects.all()
+<QuerySet [<Post: My first post>, <Post: My second post>]>  # here we can see the objects values (posts) instead of numbers
+'''        
