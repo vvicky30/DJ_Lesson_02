cc5b72488577449449a2ddcd69934e94c6c72d80
Vedant Bhatt <vickyvedant365@gmail.com>
2025-05-23 23:08:49 +0530

    this time in url file we add path/routes to post_page views with named url , named it as 'page'. Here we also designated the url file to app_name 'posts' which only saves routes to the views pages dedicated to posts django app.

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,13 @@
 from django.urls import path
 from . import views   # importing views for giving path to web pages using urlpatterns(routes)
 
+app_name = 'posts'  #here we designated the this url file as dedicated to 'posts' (django app) where it stores only urls to views of posts-app pages
+# we're doing so because in broader and larger project we're not going to make a single django-app but going to make multiple django app.
+
 urlpatterns = [
-    path('', views.posts_list, name= "posts" ),    # here we added the path/route to posts_list views which is in the views.py(dedicated to posts app only) 
+    path('', views.posts_list, name= "list" ),    # here we added the path/route to posts_list views which is in the views.py(dedicated to posts app only) 
                                                   #here we also added name paramter in giving path to views posts_list ,this because we want to make this url as named-url ,here it's named as "posts"
+    path('<slug:slug>', views.post_page, name= "page" ), # here we're adding routes to the views 'post_page' with named-url giving name as "page" for right now 
+     #here we are using slug as the path converter which will automatically adds corresponding slug to the end of the actual url last-page ex:localhost:8000/posts/first-post
+     # tis will automatically takes corresponding slug of post and adds it to the last page url because we here used slug itselg as parameter to the slug path converter.
 ]
