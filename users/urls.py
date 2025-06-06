e33df0542c67c101e56965e5333d4dde90f18d4d
Vedant Bhatt <vickyvedant365@gmail.com>
2025-06-06 21:25:22 +0530

    add path/routes to logout view with named url in urlpatterns list.

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,7 @@ app_name = 'users'  # no django app name is 'users' for which it designated
 urlpatterns = [
     path('register/', views.register_view, name="register"),# here we added the path/route to 'register_view' views which is in the views.py(dedicated to users app only) 
                                                    # here named-url we added by name 'register'
-    path('login/', views.login_view, name="login")# here we make the named url for login view just like we did in creating named url for register view
+    path('login/', views.login_view, name="login"),# here we make the named url for login view just like we did in creating named url for register view
+    path('logout/', views.logout_view, name="logout")
+    
 ]
