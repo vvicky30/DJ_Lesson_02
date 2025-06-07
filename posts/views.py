3c5046b830d392b8bf5fa2e5be79033348d26974
Vedant Bhatt <vickyvedant365@gmail.com>
2025-06-07 23:49:08 +0530

    Here we made post_new view and import 'login_required decorator' as ; this decorator used to add functionality to post_new view that we already built . this 'login_required' decorator redirect the unauthorized-user[who's not loged-in yet] to login page [which's in the 'user' djnago app] ;  and in this way it protects unauthorized user to acess 'new-post' page .

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post # importing Post data model class from models.py file 
 #from django.http import HttpResponse  # this will used to echoes-in the corresponding slug of the post to the respective post-page  [for testing] 
+from django.contrib.auth.decorators import login_required # here we import login_required decorator from django.contrib.auth [for preventing our post_new page to be view to unauthorized user]
 # Create your views here.
 def posts_list(request):
     #saving all posts-contents(Objects values) to posts-variable here
@@ -18,3 +19,13 @@ def post_page(request, slug):
     #return HttpResponse(slug)  # going to show echoed corresponding slug to post page [testing]
     post= Post.objects.get(slug=slug)  # instead of one post we're going to show single post based of corresponding slug(going to match slug to the slug given as argument to the function)
     return render(request, 'posts/post_page.html', {'post': post})
+
+
+
+# as we know in python decorators used to add functionalities to exisiting function without modifying its original code within.
+@login_required(login_url="/users/login/")# this logic_required decorator redirect the unauthorized-user[who's not loged-in yet] to login page [which's in the 'user' djnago app] ; 
+#and in this way it protects unauthorized user to acess 'new-post' page 
+# [here in case after user loged-in , it will redirect to list page of posts djnago app ; as this was done due to the redirection to post:list mentioned in login_view in 'user' app]
+#[once user logged-in instead of redirecting user to post:list page we have to redirect for which user press the icon (which was here the icon of new-post page); we can do this by adding favourable logics to login_views in user-app and alligning it with login.html file]
+def post_new(request):
+    return render(request, 'posts/post_new.html')
